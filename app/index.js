8f6cc757acf1a59dd446a9e41f215e40a5cb54fe
Brandon Ros <brandonros1@gmail.com>
2019-10-19 23:37:33 -0400

    cleaner app

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg')
+const pg = require('pg')
 const redis = require('redis')
 const express = require('express')
 const puppeteer = require('puppeteer-core')
@@ -6,24 +6,24 @@ const puppeteer = require('puppeteer-core')
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
 const initDb = async () => {
-  const dbClient = new Client({
+  const client = new pg.Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB
   })
-  await dbClient.connect()
-  return dbClient
+  await client.connect()
+  return client
 }
 
 const initCache = async () => {
-  const redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST)
+  const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST)
   await new Promise(function (resolve, reject) {
-    redisClient.on('ready', resolve)
-    redisClient.on('error', reject)
+    client.on('ready', resolve)
+    client.on('error', reject)
   })
-  return redisClient
+  return client
 }
 
 const initBrowser = async () => {
